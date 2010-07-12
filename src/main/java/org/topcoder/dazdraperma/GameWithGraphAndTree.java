0829153fe3cff11665ea7c4e267f0ad961cfe4db
Miguel Covarrubias <mlcovarrubias@gmail.com>
2010-07-12 17:34:42 -0400

    not working no idea why

--- src/main/java/org/topcoder/dazdraperma/GameWithGraphAndTree.java
+++ src/main/java/org/topcoder/dazdraperma/GameWithGraphAndTree.java
@@ -1,8 +1,6 @@
 package org.topcoder.dazdraperma;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class GameWithGraphAndTree {
@@ -13,36 +11,22 @@ public class GameWithGraphAndTree {
   public static int factorial(int n) {
     int ret = 1;
     
-    for (int i = 1; i <= n; i++)
-      ret *= n;
+    for (int i = 2; i <= n; i++)
+      ret *= i;
     
     return ret;
   }
 
-  public class PermutationGenerator<E> implements Iterator<List<E>>, Iterable<List<E>> {
+  public static class PermutationGenerator implements Iterable<int []>, Iterator<int []> {
 
     private int[] a;
     private int numLeft;
     private int total;
-    private E [] stuffToPermute;
     
-    private List<E> buffer;
-
-    // -----------------------------------------------------------
-    // Constructor. WARNING: Don't make n too large.
-    // Recall that the number of permutations is n!
-    // which can be very large, even when n is as small as 20 --
-    // 20! = 2,432,902,008,176,640,000 and
-    // 21! is too big to fit into a Java long, which is
-    // why we use BigInteger instead.
-    // ----------------------------------------------------------
-
-    public PermutationGenerator(int n, E [] stuff) {
+    public PermutationGenerator(int n) {
       if (n < 0) {
         throw new IllegalArgumentException("Min 0");
       }
-      stuffToPermute = stuff;
-      buffer = new ArrayList<E>(stuff.length);
       a = new int[n];
       total = factorial(n);
       reset();
@@ -63,22 +47,15 @@ public class GameWithGraphAndTree {
     // Generate next permutation (algorithm from Rosen p. 284)
     // --------------------------------------------------------
     
-    private List<E> returnPermutation() {
-      for (int i = 0; i < a.length; i++)
-        buffer.set(i, stuffToPermute[a[i]]);
-      
-      return buffer;  
-      
-    }
 
-    public List<E> next() {
+    public int [] next() {
 
       if (numLeft == total) {
         numLeft--;
-        return returnPermutation();
+        return a;
       }
       
-      if (numLeft < 0)
+      if (numLeft <= 0)
         throw new NoSuchElementException();
 
       int temp;
@@ -118,23 +95,22 @@ public class GameWithGraphAndTree {
       }
 
       numLeft--;
-      return returnPermutation();
+      return a;
 
-    }
-
-
-    public Iterator<List<E>> iterator() {
-      return this;
     }
 
     public boolean hasNext() {
-      return numLeft >= 0;
+      return numLeft > 0;
     }
 
 
     public void remove() {
       throw new UnsupportedOperationException();
       
+    }
+
+    public Iterator<int[]> iterator() {
+      return this;
     }
   }
   
@@ -143,8 +119,11 @@ public class GameWithGraphAndTree {
     
     public int [] [] packing;
     
+    public static int NEGATORY; 
+    
     public Graph(String [] graph) {
       packing = new int[graph.length][(graph.length + 1)/2];
+      NEGATORY = ((int) Math.pow(2, packing.length)) - 1;
       
       for (int g = 0; g < graph.length; g++) {
         for (int c = 0; c < graph.length; c++) {
@@ -156,23 +135,47 @@ public class GameWithGraphAndTree {
     }
     
     /**
-     * Can this other Pack be overlaid upon us?
+     * Can this other Graph be overlaid upon us?
      * @param other
      * @return
      */
     public boolean canOverlay(Graph other) {
      
-      int negatory = ((int) Math.pow(2, packing.length)) - 1;
-      
       for (int g = 0; g < packing.length; g++) {
         for (int c = 0; c < packing[g].length; c++) {
-          if (((packing[g][c] ^ negatory) & other.packing[g][c]) != 0)
+          if (((packing[g][c] ^ NEGATORY) & other.packing[g][c]) != 0)
             return false;
         }
       }
       
       return true;
-    }    
+    }
+    
+    
+    public int count(Graph other) {
+      
+      PermutationGenerator permuter = new PermutationGenerator(packing.length);
+      int count = 0;
+      
+      int [] [] permutation = new int[packing.length][];
+      int [] [] tmp;
+      
+      
+      for (int [] a : permuter) {
+        for (int i = 0; i < packing.length; i++)
+          permutation[i] = packing[a[i]];
+      
+        tmp = packing;
+        packing = permutation;
+        if (this.canOverlay(other))
+          count++;
+        packing = tmp;
+      }
+      
+      return count;
+      
+    }
+    
   }
   
   
@@ -183,9 +186,8 @@ public class GameWithGraphAndTree {
     Graph g = new Graph(graph);
     
     Graph t = new Graph(tree);
-    
-    int count = 0;
-    return count;
+
+    return g.count(t);
   }
 
 }
